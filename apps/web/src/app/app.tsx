11ae76ed9d96543ab9d2de6eec2bb33adc78aeb2
Jeffrey Hsiung <jeffrey@bearlybits.com>
2019-11-24 15:25:04 -0800

    set up material ui and basic app layout

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,286 +1,78 @@
-import React from 'react';
-
-import styled from '@emotion/styled';
-
-import { Route, Link } from 'react-router-dom';
-
-const StyledApp = styled.div`
-  /*
- * Remove template code below
- */
-
-  font-family: sans-serif;
-  min-width: 300px;
-  max-width: 600px;
-  margin: 50px auto;
-
-  .gutter-left {
-    margin-left: 9px;
-  }
-
-  .col-span-2 {
-    grid-column: span 2;
-  }
-
-  .flex {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  header {
-    background-color: #143055;
-    color: white;
-    padding: 5px;
-    border-radius: 3px;
-  }
-
-  main {
-    padding: 0 36px;
-  }
-
-  p {
-    text-align: center;
-  }
-
-  h1 {
-    text-align: center;
-    margin-left: 18px;
-    font-size: 24px;
-  }
-
-  h2 {
-    text-align: center;
-    font-size: 20px;
-    margin: 40px 0 10px 0;
-  }
-
-  .resources {
-    text-align: center;
-    list-style: none;
-    padding: 0;
-    display: grid;
-    grid-gap: 9px;
-    grid-template-columns: 1fr 1fr;
-  }
-
-  .resource {
-    color: #0094ba;
-    height: 36px;
-    background-color: rgba(0, 0, 0, 0);
-    border: 1px solid rgba(0, 0, 0, 0.12);
-    border-radius: 4px;
-    padding: 3px 9px;
-    text-decoration: none;
-  }
-
-  .resource:hover {
-    background-color: rgba(68, 138, 255, 0.04);
-  }
-
-  pre {
-    padding: 9px;
-    border-radius: 4px;
-    background-color: black;
-    color: #eee;
-  }
-
-  details {
-    border-radius: 4px;
-    color: #333;
-    background-color: rgba(0, 0, 0, 0);
-    border: 1px solid rgba(0, 0, 0, 0.12);
-    padding: 3px 9px;
-    margin-bottom: 9px;
-  }
-
-  summary {
-    outline: none;
-    height: 36px;
-    line-height: 36px;
-  }
-
-  .github-star-container {
-    margin-top: 12px;
-    line-height: 20px;
-  }
-
-  .github-star-container a {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    color: #333;
-  }
-
-  .github-star-badge {
-    color: #24292e;
-    display: flex;
-    align-items: center;
-    font-size: 12px;
-    padding: 3px 10px;
-    border: 1px solid rgba(27, 31, 35, 0.2);
-    border-radius: 3px;
-    background-image: linear-gradient(-180deg, #fafbfc, #eff3f6 90%);
-    margin-left: 4px;
-    font-weight: 600;
-  }
-
-  .github-star-badge:hover {
-    background-image: linear-gradient(-180deg, #f0f3f6, #e6ebf1 90%);
-    border-color: rgba(27, 31, 35, 0.35);
-    background-position: -0.5em;
-  }
-  .github-star-badge .material-icons {
-    height: 16px;
-    width: 16px;
-    margin-right: 4px;
-  }
-`;
+/** @jsx jsx */
+import { Global, jsx } from '@emotion/core';
+import { useTheme, makeStyles, createStyles } from '@material-ui/styles';
+import {
+  AppBar,
+  Toolbar,
+  Avatar,
+  Typography,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+import VideogameAssetIcon from '@material-ui/icons/VideogameAsset';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import { green } from '@material-ui/core/colors';
 
 export const App = () => {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./${fileName}.${style} file.
-   */
+  const theme: any = useTheme();
+  const useStyles = makeStyles(theme =>
+    createStyles({ paper: { width: 240 } })
+  );
+  const classes = useStyles({});
+
   return (
-    <StyledApp>
-      <header className="flex">
-        <img
-          alt=""
-          width="75"
-          src="https://nx.dev/assets/images/nx-logo-white.svg"
-        />
-        <h1>Welcome to web!</h1>
-      </header>
-      <main>
-        <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx.</p>
-        <div className="flex github-star-container">
-          <a
-            href="https://github.com/nrwl/nx"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div css={{ display: 'flex' }}>
+      <Global
+        styles={{ body: { backgroundColor: theme.palette.background.default } }}
+      />
+      <AppBar css={{ zIndex: theme.zIndex.drawer + 1 }} position="fixed">
+        <Toolbar css={{ display: 'flex' }}>
+          <div css={{ flexGrow: 1 }}>
+            <Typography variant="h6">Avalon</Typography>
+          </div>
+          <Button
+            variant="contained"
+            color="secondary"
+            css={{ marginRight: theme.spacing(2) }}
           >
-            {' '}
-            If you like Nx, please give it a star:
-            <div className="github-star-badge">
-              <svg
-                className="material-icons"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-              </svg>
-              Star
-            </div>
-          </a>
-        </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
-            <li className="col-span-2">
-              <a
-                className="resource flex"
-                href="https://connect.nrwl.io/app/courses/nx-workspaces/intro"
-              >
-                Nx video course
-              </a>
-            </li>
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/getting-started/what-is-nx"
-            >
-              Nx video tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/tutorial/01-create-application"
-            >
-              Interactive tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a className="resource flex" href="https://connect.nrwl.io/">
-              <img
-                height="36"
-                alt="Nrwl Connect"
-                src="https://connect.nrwl.io/assets/img/CONNECT_ColorIcon.png"
-              />
-              <span className="gutter-left">Nrwl Connect</span>
-            </a>
-          </li>
-        </ul>
-        <h2>Next Steps</h2>
-        <p>Here are some things you can do with Nx.</p>
-        <details open>
-          <summary>Add UI library</summary>
-          <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
-
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-        </details>
-        <details>
-          <summary>View dependency graph</summary>
-          <pre>{`nx dep-graph`}</pre>
-        </details>
-        <details>
-          <summary>Run affected commands</summary>
-          <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-`}</pre>
-        </details>
-      </main>
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route
-        path="/page-2"
-        exact
-        render={() => (
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        )}
-      />
-      {/* END: routes */}
-    </StyledApp>
+            Create Game
+          </Button>
+          <Avatar
+            css={{
+              color: '#fff',
+              backgroundColor: green[500]
+            }}
+          >
+            JH
+          </Avatar>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        classes={{
+          paper: classes.paper
+        }}
+        open={true}
+        variant="persistent"
+      >
+        <div css={theme.mixins.toolbar}></div>
+        <List>
+          {[
+            { icon: <VideogameAssetIcon />, text: 'Find Games' },
+            { icon: <ShowChartIcon />, text: 'Stats' }
+          ].map((item, index) => (
+            <ListItem button key={item.text}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main css={{ flexGrow: 1 }}>blah </main>
+    </div>
   );
 };
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { StylesProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <CssBaseline />
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={createMuiTheme({ palette: { type: 'dark' } })}>
+        <App />
+      </ThemeProvider>
+    </StylesProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
